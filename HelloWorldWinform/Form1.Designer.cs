3fb8ced0e5b7fd984f42fc85a752a771613c2982
Darecoco <whaqlrh37@naver.com>
2024-03-25 14:27:58 +0900

    Form 제목 변경

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(402, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form123456789";
             this.ResumeLayout(false);
 
         }
